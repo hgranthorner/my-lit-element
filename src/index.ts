2fc24f5392c2daf8d0e8b68076222a34e33e332c
Grant Horner <h.grant.horner@gmail.com>
2019-09-22 17:48:26 -0400

    set up

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { LitElement, html, property, customElement } from 'lit-element';
+
+@customElement('simple-greeting')
+export class SimpleGreeting extends LitElement {
+  @property() name = 'World';
+
+  render() {
+    return html`<p>Hello, ${this.name}!</p>`;
+  }
+}
+
+
